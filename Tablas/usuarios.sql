a20c95d0357ae4d2aee02dfe89e7ba261a11f00a
HoldenAndy <kaziroz09@gmail.com>
2026-01-13 21:13:41 -0500

    Añadir campos en usuarios.

--- Tablas/usuarios.sql
+++ Tablas/usuarios.sql
@@ -5,5 +5,8 @@ CREATE TABLE IF NOT EXISTS usuarios (
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL,
     activado BOOLEAN DEFAULT FALSE,
-    codigo_verificacion VARCHAR(255)
+    codigo_verificacion VARCHAR(255),
+    codigo_verificacion_expiracion DATETIME DEFAULT NULL,
+    token_recuperacion_password VARCHAR(255) DEFAULT NULL,
+    token_expiracion_password DATETIME DEFAULT NULL
 );
